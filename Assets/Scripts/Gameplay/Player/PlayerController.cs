e3b6296aebc8103b479d144e80f29221111e3ae4
Leo WILS <wilsleo.plp@gmail.com>
2016-09-03 21:03:05 +0200

    setup for the 3-casts character physic system

--- Assets/Scripts/Gameplay/Player/PlayerController.cs
+++ Assets/Scripts/Gameplay/Player/PlayerController.cs
@@ -8,6 +8,14 @@ public class PlayerController : MonoBehaviour {
     private Transform _transform;
     private BoxCollider2D _collider;
 
+    public Vector2 Position2D
+    {
+        get
+        {
+            return _transform.position;
+        }
+    }
+
     // Use this for initialization
     void Awake()
     {
@@ -18,7 +26,10 @@ public class PlayerController : MonoBehaviour {
     // Use this for initialization
     void Start()
     {
-        
+        sizeX = Vector2.right * _collider.size.x;
+        sizeY = Vector2.up * _collider.size.y;
+        extentX = sizeX * 0.5f;
+        extentY = sizeY * 0.5f;
     }
 
     private Vector2 tempVector;
@@ -34,50 +45,39 @@ public class PlayerController : MonoBehaviour {
     private LayerMask groundCastLayer;
     private RaycastHit2D hit;
 
-    private Vector2 movementAnchor;
+    private Vector2 movementAnchor; //debug draw purpose
+    //private Vector2 previousPosition;
+
     private Vector2 movementVector;
-    private Vector2 previousPosition;
+    private int horizontalFactor = 1;
+    private int verticalFactor = 1;
+    private Vector2 extentX; 
+    private Vector2 extentY;
+    private Vector2 sizeX;
+    private Vector2 sizeY;
+
+    private Vector2 mainCastTarget; // main corner position. normally, only this one should need to return a RaycastHit2D
+    private Vector2 frontCastTarget; // testing wall
+    private Vector2 backCastTarget; // testing ground or ceiling
+                                                                               
 
     // Update is called once per frame
     void Update()
     {
-        if(movementVector.magnitude > 0f) // if you must, move it!
-        {
-            hit = Physics2D.BoxCast((Vector2)_transform.position, _collider.size, 0f, movementVector, movementVector.magnitude * Time.deltaTime, groundCastLayer);
 
-            movementAnchor = movementVector * (_collider.bounds.extents.magnitude / movementVector.magnitude);
-            Debug.DrawLine((Vector2)_transform.position, (Vector2)_transform.position + movementAnchor + movementVector * Time.deltaTime, Color.green);
+    }
 
-            if (hit.centroid.magnitude > 0f) // hit something
-            {
-                if (Vector2.Angle(hit.normal, Vector2.up) == 90f) // wall
-                {
-                    tempFloat = _transform.position.y + (movementVector * Time.deltaTime).y - hit.centroid.y;
-                    tempVector = new Vector2(0f, tempFloat);//.up * tempFloat;
-                    _transform.position = hit.centroid;
+    void CalculateFacing()
+    {
+        horizontalFactor =  movementVector.x >= 0f ? 1 : -1;
+        verticalFactor =    movementVector.y > 0f  ? 1 : -1;
+    }
 
-                    hit = Physics2D.Raycast((Vector2)_transform.position, tempVector, tempVector.magnitude, groundCastLayer);
-
-                    movementAnchor = tempVector * (_collider.bounds.extents.magnitude / tempVector.magnitude);
-                    Debug.DrawLine((Vector2)_transform.position, (Vector2)_transform.position + movementAnchor + tempVector, Color.blue);
-
-                    if (hit.centroid.magnitude > 0f) // hit something again, no second check
-                    {
-                        _transform.position = hit.point - _collider.offset;
-                    }
-                    else
-                    {
-                        _transform.Translate(tempVector);
-                    }
-                }
-                else
-                { /*if(Vector2.Angle(hit.normal, Vector2.up) == 0f)*/ // grounded flat
-                    _transform.position = hit.centroid;
-                }
-            }
-            else
-                _transform.Translate(movementVector * Time.deltaTime);
-        }
+    void PlaceCastTarget()
+    {
+        mainCastTarget = Position2D + movementVector*Time.deltaTime + extentX*horizontalFactor + extentY*verticalFactor;
+        frontCastTarget = mainCastTarget + sizeY*verticalFactor;
+        backCastTarget = mainCastTarget + sizeX*horizontalFactor;
     }
 
     #endregion
