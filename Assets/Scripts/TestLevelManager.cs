8106ba81dad52f625b1f086aff13f33728c45f1f
Leo WILS <wilsleo.plp@gmail.com>
2016-09-20 20:36:36 +0200

    a way to load scene with ui buttons

---
+++ Assets/Scripts/TestLevelManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class TestLevelManager : MonoBehaviour {
+
+    public static TestLevelManager Instance;
+    public string[] levels;
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    public void GoToLevel(int levelID)
+    {
+        SceneManager.LoadScene(levelID);
+    }
+    public void GoToLevel(string levelID)
+    {
+        SceneManager.LoadScene(levelID);
+    }
+}
