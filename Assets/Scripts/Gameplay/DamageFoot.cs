1bb19200b92b945447ab351e9674b7d6494763a4
Pedro Dominicé <helvest@outlook.com>
2016-10-20 15:02:43 +0200

    rebond après jumps sur ennemi

---
+++ Assets/Scripts/Gameplay/DamageFoot.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DamageFoot : DamageObject
+{
+	PlayerController player;
+
+	// Use this for initialization
+	void Start () {
+		player = GetComponentInParent<PlayerController>();
+	}
+
+	protected override void GetDamage()
+	{
+		player.AfterFootTouch();
+	}
+}
